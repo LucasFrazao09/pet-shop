c48c568d67b98cec18c50ca307e13863b098a474
Lucas <lucas_af_bezerra@estudante.sesisenai.org.br>
2026-08-25 11:02:49 -0300

    feat - banco de dados e conexão

---
+++ database/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE pet_shop_frazao;
+USE pet_shop_frazao;
+
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE cachorros (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cachorro VARCHAR(100) NOT NULL,
+    usuario_id INT, FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    raca_cachorro VARCHAR(100) NOT NULL,
+    idade_cachorro INT,
+    tipo_cachorro VARCHAR(100)
+);
